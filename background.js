c949fa3b1957de2479b30ed62b0e6497443f79bd
hai <hai842@gmail.com>
2023-05-12 19:06:11 +0300

    final update

--- background.js
+++ background.js
@@ -7,19 +7,28 @@ chrome.alarms.onAlarm.addListener(() => {
   const URL = 'https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=en&mode=1'; // Replace with your API endpoint
 
   fetchData(URL)
-    .then(result => {
-      const message = JSON.stringify(result); // Convert the array to a string
+  .then(result => {
+    let message;
+    if (result.length > 0) {
+      message = result.map(item => `${item.alertDate}: ${item.data}`).join('\n');
+    } else {
+      message = 'null';
+    }
+    if (message !== 'null') {
       chrome.notifications.create({
         type: 'basic',
         iconUrl: 'alarm.jpg',
-        title: 'Stay Hydrated',
+        title: 'Rockets Alert!',
         message: message,
         silent: false
       }, () => {});
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
 });
 
 function formatDate(dateString) {
@@ -41,7 +50,7 @@ function isAlertDateTimeInRange(alertDate) {
   const currentDateTime = new Date(`${currentDateString} ${currentTimeString}`);
 
   const diffInMinutes = Math.abs(alertDateTime - currentDateTime) / (1000 * 60);
-  return diffInMinutes <= 90;
+  return diffInMinutes <= 150;
 }
 
 function fetchData(URL) {
@@ -82,10 +91,10 @@ chrome.runtime.onMessage.addListener(
 
 function createAlarm() {
   chrome.alarms.create(
-    "drink_water",
+    "Rockets Alert!",
     {
-      delayInMinutes: 1,
-      periodInMinutes: 1
+      delayInMinutes: 0.33,
+      periodInMinutes: 0.33
     }
   );
 }
